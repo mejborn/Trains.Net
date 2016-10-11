ea19dd6ea3edf121a07fdd4727ecd2ec69e36a8f
BudgieFreak3000 <simojp95@gmail.com>
2016-10-11 11:11:15 +0200

    Did things....

--- Model/Class1.cs
+++ Model/Class1.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Class1
+
+    public interface IDataModel
     {
+        List<IStation> GetStations();
+        void AddStation(IStation station);
+        void RemoveStation(Station station);
     }
+
+
+    public interface IStation { }
+    public class Station { }
 }
--- ViewModel/Class1.cs
+++ ViewModel/Class1.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+
 
 namespace ViewModel
 {
-    public class Class1
+    public class ViewModel
     {
+        private IDataModel idm;
+
+        public ViewModel()
+        {
+            idm.GetStations();
+            idm.RemoveStation(new Station());
+        }
     }
 }
